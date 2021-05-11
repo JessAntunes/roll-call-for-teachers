41b8280efb031a25fb45f8ebd4c73e9cb72f8293
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:37:37 -0400

    create fullname and last_full methods for lecturer and student to display names

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -2,6 +2,11 @@ class Lecturer < ApplicationRecord
     has_secure_password
     has_many :courses
     has_many :students, through: :courses
+    validates_presence_of :first_name, :last_name, :email
+    validates_uniqueness_of :email
 
+    def fullname
+        "#{self.first_name} #{self.last_name}"
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
     has_many :courses
     has_many :lecturers, through: :courses
     
+    def last_first
+        "#{self.last_name}, #{self.first_name}"
+    end
+    
 end
