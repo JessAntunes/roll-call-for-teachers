0ae0f3f1a9415ce7a7d0f325ed2ecd490be6ebdb
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:14:54 -0400

    change all 'user' to lecturer

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
     end
   
     def create
-      @user = User.find_by(email: params[:email])
-      if @user && @user.authenticate(params[:password])
-        session[:user_id] = @user.id
-        redirect_to @user
+      @lecturer = Lecturer.find_by(email: params[:email])
+      if @lecturer && @lecturer.authenticate(params[:password])
+        session[:lecturer_id] = @lecturer.id
+        redirect_to @lecturer
       else
         flash[:danger] = 'Invalid email/password combination' 
         render 'new'
@@ -18,5 +18,5 @@ class SessionsController < ApplicationController
       session.clear
       redirect_to login_path
     end
-    
+
 end
