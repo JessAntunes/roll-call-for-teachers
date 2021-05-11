248ec59a79beeeeaf37205b1aa384b9a69d01066
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:06:14 -0400

    add controllers to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+  
+    def create
+      @user = User.find_by(email: params[:email])
+      if @user && @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        flash[:danger] = 'Invalid email/password combination' 
+        render 'new'
+      end
+    end
+  
+    def destroy
+      session.clear
+      redirect_to login_path
+    end
+    
 end
