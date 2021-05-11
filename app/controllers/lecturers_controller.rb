b4d56f3c36cbd1fa01f1b999a201c43b43c7fbae
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:16:58 -0400

    add lecturer controllers

--- app/controllers/lecturers_controller.rb
+++ app/controllers/lecturers_controller.rb
@@ -1,2 +1,67 @@
 class LecturersController < ApplicationController
+    before_action :find_lecturer, only: [:show, :edit, :update, :destroy]
+    
+  
+    def new
+        @lecturer = Lecturer.new
+    end
+
+    def show
+        @current_lecturer
+    end
+  
+    def create
+        @lecturer = Lecturer.new(lecturer_params)
+        if @lecturer.save
+            session[:lecturer_id] = @lecturer.id
+            redirect_to @lecturer
+        else
+            render :new
+        end
+    end 
+
+    def show
+        @current_lecturer
+        if !@lecturer
+            redirect_to wrong_page
+        end
+    end
+  
+    def edit 
+        if !@lecturer
+            redirect_to wrong_page
+        end
+    end 
+  
+    def update
+        if @lecturer
+            @lecturer.update(lecturer_params)
+            if @lecturer.errors.any?
+                render :edit
+            else
+                redirect_to @lecturer
+            end
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @lecturer.destroy
+        redirect_to "/home"
+    end
+
+    private
+
+    def lecturer_params
+        params.require(:lecturer).permit(:first_name,
+                                    :last_name
+                                    :email, 
+                                    :password, 
+                                    )
+    end 
+
+    def find_lecturer
+        @lecturer = Lecturer.find_by_id(params[:id])
+    end 
 end
