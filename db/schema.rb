ad47e7cdd521957840513c99860d5b0f10ab56a3
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 21:38:06 -0400

    migrate tables

---
+++ db/schema.rb
@@ -0,0 +1,44 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_11_013158) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "subject"
+    t.integer "student_id", null: false
+    t.integer "lecturer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["lecturer_id"], name: "index_courses_on_lecturer_id"
+    t.index ["student_id"], name: "index_courses_on_student_id"
+  end
+
+  create_table "lecturers", force: :cascade do |t|
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "courses", "lecturers"
+  add_foreign_key "courses", "students"
+end
