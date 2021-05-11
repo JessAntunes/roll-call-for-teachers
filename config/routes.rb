e9f1f5ebae6abb6bb90147aad814fd217eda4e1b
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 23:13:14 -0400

    add missing edit route for courses in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :courses, only [:index, :show]
   resources :students
   resources :lecturers do
-    resources :courses, only: [:new, :create, :update, :destroy]
+    resources :courses, only: [:new, :edit, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
