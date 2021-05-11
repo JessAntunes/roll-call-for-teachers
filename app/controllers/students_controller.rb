3385c46417aa0db7bd515fbb03968741ac7afff9
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:21:09 -0400

    add create controller to student controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,4 +10,13 @@ class StudentsController < ApplicationController
     def show
     end
 
+    def create
+        @student = Student.new(student_params)
+        if @student.save
+            redirect_to student_path(@student)
+        else
+            render :new
+        end
+    end 
+
 end
