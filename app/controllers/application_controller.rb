402969c3b5a512569884a1a0388b743a6a6c6e85
Jessica Antunes <jessantunes@protonmail.com>
2021-05-10 22:12:29 -0400

    add controllers to application controller and add home view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :logged_in?
+    helper_method :current_user
+
+    def home
+        render "/home"
+    end
+
+    def wrong_page
+        render "/wrong_page"
+    end
+
+
+
+
+    def current_user
+        @user ||= User.find_by_id(session[:user_id])
+    end
+    
+    def logged_in?
+        !current_user.nil?
+    end
+    
 end
