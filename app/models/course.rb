45a64d47225d472e26e48ebeebd32549348de7ea
Jessica Antunes <jessantunes@protonmail.com>
2021-05-11 01:30:28 -0400

    add students method to course and create show view

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,13 @@
 class Course < ApplicationRecord
   belongs_to :student
   belongs_to :lecturer
+
+  def students
+    list = []
+    self.student.each do |s|
+      list << Student.find_by_id(s)
+    end
+    list
+  end
+  
 end
