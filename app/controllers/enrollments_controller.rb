313eb7c021173cab2274dad5676036ec56990972
Jessica Antunes <jessantunes@protonmail.com>
2021-05-12 01:11:52 -0400

    create enrollment show page

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,2 +1,47 @@
 class EnrollmentsController < ApplicationController
+    before_action :find_enrollment, only: [:show, :edit, :update, :destroy]
+  
+    def new
+    end
+
+    def show
+    end
+
+    def create
+        @enrollment = Enrollment.new(enrollment_params)
+        if @enrollment.save
+            redirect_to student_path(@enrollment.student)
+        else
+            render :new
+        end
+    end 
+
+    def edit 
+    end 
+  
+    def update
+        @enrollment.update(enrollment_params)
+        if @enrollment.save
+            redirect_to student_path(@enrollment.student)
+        else
+            render :edit
+        end 
+    end
+
+    def delete
+        @enrollment.destroy
+        redirect_to student_path(@enrollment.student)
+    end
+
+    private
+
+    def enrollment_params
+        params.require(:enrollment).permit(:grade, :course_id, :student_id)
+    end 
+
+    def find_enrollment
+        @enrollment = Enrollment.find_by_id(params[:id])
+    end 
+
+
 end
