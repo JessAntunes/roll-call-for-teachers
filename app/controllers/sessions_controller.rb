75e43871f7790f7bb582d5e02781663232205b02
Jessica Antunes <jessantunes@protonmail.com>
2021-05-12 22:25:21 -0400

    update session controller to separate facebook create and reg login create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,29 +1,31 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :verified_lecturer, only: [:new, :create]
     def new
     end
     
     
     def create
-      if  params[:email]
-        @lecturer = Lecturer.find_by(email: params[:email])
-        if @lecturer && @lecturer.authenticate(params[:password])
+      @lecturer = Lecturer.new(lecturer_params)
+      if @lecturer.save 
           session[:lecturer_id] = @lecturer.id
-          redirect_to @lecturer
-        else
-          flash[:danger] = 'Invalid email/password combination' 
+          redirect_to movies_path
+      else 
+          @error = @lecturer.errors.full_messages
           render :new
-        end
-      else
-        @lecturer = Lecturer.find_or_create_by(uid: auth['uid']) do |l|
-          l.first_name = auth['info']['name'].split(' ')[0]
-          l.last_name = auth['info']['name'].split(' ')[1]
-          l.email = auth['info']['email']
-        end
-        session[:lecturer_id] = @lecturer.id
-        redirect_to @lecturer
       end
     end
+
+    def facebook
+      @lecturer = Lecturer.find_or_create_by(email: auth["info"]["email"]) do |lecturer| 
+          lecturer.password =  SecureRandom.hex(10)
+      end 
+      if @lecturer && @lecturer.id
+          session[:lecturer_id] = @lecturer.id
+          redirect_to movies_path 
+      else 
+          redirect_to "/login"
+      end 
+  end 
   
     def destroy
       session.clear
@@ -35,5 +37,9 @@ class SessionsController < ApplicationController
   def auth
     request.env['omniauth.auth']
   end
+
+  def lecturer_params
+    params.require(:lecturer).permit(:email, :password)
+  end 
 
 end
