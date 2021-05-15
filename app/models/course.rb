0169f931633c05bd94356002cbd0c49c4caeb6e1
Jessica Antunes <jessantunes@protonmail.com>
2021-05-14 21:41:26 -0400

    add validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,7 @@ class Course < ApplicationRecord
   belongs_to :lecturer
   has_many :enrollments, dependent: :destroy
   has_many :students, through: :enrollments
+  validates_presence_of :subject
   accepts_nested_attributes_for :students, reject_if: :all_blank
 
   scope :order_by_subject, -> {order(subject: :asc)}
